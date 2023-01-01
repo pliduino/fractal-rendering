Small fixes + better color gradient

python-rendering/main.py
@@ -1,5 +1,4 @@
 import dearpygui.dearpygui as dpg
-import pixie
 import numpy as np
 
 # TODO: Implement in C++ for performance
@@ -7,7 +6,7 @@ import numpy as np
 
 def CalcMandelbrot(x, y, iterations, escape_constant):
     constant = np.complex128(x + (y*1j))
-    nextZ = np.complex128(0 + 0j)
+    nextZ = constant  # Skipping first iteration
 
     for i in range(iterations):
         nextZ = (nextZ*nextZ) + constant
@@ -19,21 +18,36 @@ def CalcMandelbrot(x, y, iterations, escape_constant):
 
 def RenderTexture(imgSize=256, iterations=3, offset=[0, 0], step=1, escape_constant=50):
     texture_data = []
+    c = 1
 
     for i in range(0, imgSize * imgSize):
-        x = (i % imgSize - (imgSize/2) - offset[0])*step
-        y = (np.floor_divide(i, imgSize) - (imgSize/2) - offset[1])*step
+        x = (i % imgSize - (imgSize/2))*step - offset[0]
+        y = (np.floor_divide(i, imgSize) - (imgSize/2))*step - offset[1]
 
         escape_time = CalcMandelbrot(x, y, iterations, escape_constant)
-        if escape_time == iterations:
-            red, green, blue = 0, 0, 0
+        if escape_time > iterations/2:
+            factor = ((iterations - 1)-(escape_time))/(iterations - 1)
+
+            red, green, blue = factor * \
+                (200/255), factor * (25/255), factor * (25/255)
         else:
-            red, green, blue = 0, 0, (iterations-escape_time)/iterations
+            factor = ((iterations - 1)-(escape_time))/(iterations - 1)
+            factor = (factor - 0.5)*2
+
+            red, green, blue = factor * \
+                (200/255), factor * (25/255), factor * (25/255)
+
+            red, green, blue = red + (factor*(-200/255)), green + (
+                factor*(+75/255)), + (factor*(+230/255))
 
         texture_data.append(red)
         texture_data.append(green)
         texture_data.append(blue)
         texture_data.append(255 / 255)
+        if (i+1 == imgSize*c):
+            print(
+                f"Progress: {'%.2f' % (((i)/((imgSize*imgSize))) * 100)}%")
+            c += 1
 
     return texture_data
 
@@ -45,6 +59,8 @@ def SaveBtn_callback(sender, app_data, user_data):
     y = dpg.get_value(user_data[3])
     step = 1/dpg.get_value(user_data[4])
     escape = dpg.get_value(user_data[5])
+
+    print("Rendering...")
 
     new_texture = RenderTexture(
         imgSize=size, iterations=iterations, offset=[x, y], step=step, escape_constant=escape)
@@ -58,6 +74,8 @@ def Init(imgSize):
     dpg.create_context()
 
     print("Initializing...")
+
+    print("Rendering...")
     texture_data = RenderTexture(imgSize)
     print("Image Rendered")
 
@@ -76,10 +94,10 @@ def Init(imgSize):
             y_slider = dpg.add_input_float(
                 label="Y Offset", default_value=0)
             step_slider = dpg.add_input_int(
-                label="Step", default_value=1)
+                label="Pixel Step", default_value=1)
             iterations_slider = dpg.add_input_int(
                 label="Iterations", default_value=3)
-            escape_slider = dpg.add_input_int(
+            escape_slider = dpg.add_input_float(
                 label="Escape Value", default_value=50)
             saveBtn = dpg.add_button(label="Save Values")
 
@@ -94,6 +112,8 @@ def Init(imgSize):
     dpg.show_viewport()
     dpg.start_dearpygui()
     dpg.destroy_context()
+
+    print("Initialized")
 
 
 Init(1024)